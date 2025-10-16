b5a6eb5a5656c10c3b0f89543ec4fa651de2e679
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-16 21:21:25 +0200

    Add Header component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p><Link href="/about">About</Link></p>
     </main>
