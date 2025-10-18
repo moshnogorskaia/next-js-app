5df85cdfd1ec22e917435ed03a52b0bdf3c241a3
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-18 18:56:54 +0200

    Prepare layout for meal app

--- app/blog/[slug]/page.js
+++
@@ -1,8 +0,0 @@
-export default function BlogPostPage({ params }) {
-    return (
-        <main>
-            <h1>Blog Post</h1>
-            <p>This is the blog post page for {params.slug}</p>
-        </main>
-    );
-}
