71a69c59f9e468d3edeb5b0f96d124f937f3658f
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-18 20:29:08 +0200

    Use NextJS Image

--- components/main-header.js
+++ components/main-header.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import classes from './main-header.module.css';
+import Image from "next/image";
 
 export default function MainHeader() {
   return <header className={classes.header}>
     <Link href="/" className={classes.logo}>
-        <img src={logo.src} alt='NextLevel Food logo' />
+        <Image src={logo} alt='NextLevel Food logo' priority />
         NextLevel Food
     </Link>
     <nav className={classes.nav}>
