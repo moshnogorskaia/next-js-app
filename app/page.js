651b981e7912420537b4066b092ea1a2720b99f9
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-19 12:34:03 +0200

    Add client component - slideshow

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
     <header className={classes.header}>
-      <div className={classes.slideShow}>
+      <div className={classes.slideshow}>
         <ImageSlideshow />
       </div>
       <div>
---
+++ components/main-header/main-header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import classes from './main-header.module.css';
+import Image from "next/image";
+import MainHeaderBackground from './main-header-background';
+
+export default function MainHeader() {
+  return (<>
+  <MainHeaderBackground />
+  <header className={classes.header}>
+    <Link href="/" className={classes.logo}>
+        <Image src={logo} alt='NextLevel Food logo' priority />
+        NextLevel Food
+    </Link>
+    <nav className={classes.nav}>
+      <ul>
+        <li>
+          <Link href="/meals">Browse Meals</Link>
+        </li>
+        <li>
+          <Link href="/community">Foodies Community</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  </>);
+}
