3455cbb81c28c85f2b19ae11c5af94ca44655b49
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-21 21:07:07 +0200

    Add Grid and Item components

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,20 @@
+import classes from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meals-grid';
+
 export default function MealsPage() {
-  return <div>MealsPage</div>;
+  return <>
+  <header className={classes.header}>
+    <h1>
+      Delicious meals, created{` `}
+      <span className={classes.highlight}>by you</span>
+    </h1>
+    <p>Browse the meals created by our community - and add your own!</p>
+    <p className={classes.cta}><Link href="/meals/share">Share your own meal</Link></p>
+  </header>
+  <main className={classes.main}>
+    
+<MealsGrid meals={[]} />
+  </main>
+  </>;
 }
