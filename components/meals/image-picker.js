f1235bc51beead9d1b9a6292c3991b9acf9934ad
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-25 13:38:48 +0200

    Trigger Image Picker on click on a button

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -6,17 +6,15 @@ import classes from './image-picker.module.css';
 export default function ImagePicker({ label, name}) {
     const inputRef = useRef(null);
 
-    function handlePckClick(event) {
-        event.preventDefault();
-        const input = document.getElementById('image');
-        input.click();
+    function handlePckClick() {
+        inputRef.current.click();
     }
 
   return (
     <div className={classes.picker}>
         <label htmlFor="image">{label}</label>
         <div className={classes.controls}>
-            <input type="file" accept="image/*"  id="image" name={name} className={classes.input} />
+            <input type="file" accept="image/*"  id="image" name={name} className={classes.input} ref={inputRef} />
             <button type="button" className={classes.button} onClick={handlePckClick}>Upload</button>
         </div>
     </div>
