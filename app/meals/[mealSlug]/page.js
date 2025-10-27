330dfecad7d4bff8e2fd19a5118b3009e7eeb275
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-10-27 21:10:35 +0100

    Store images in AWS

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,7 @@
-import classes from './page.module.css';
-import Image from 'next/image';
-import { getMeal } from '@/lib/meals';
-import { notFound } from 'next/navigation';
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
@@ -10,25 +10,32 @@ export default function MealDetailsPage({ params }) {
     notFound();
   }
 
-  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
   return (
     <>
-    <header className={classes.header}>
-      <div className={classes.image}>
-        <Image src={meal.image} alt={meal.title} fill />
-      </div>
-      <div className={classes.headerText}>
-        <h1>{meal.title}</h1>
-        <p className={classes.creator}>
-          by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-        </p>
-        <p className={classes.summary}>{meal.summary}</p>
-      </div>
-    </header>
-    <main className={classes.main}>
-      <p className={classes.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
-    </main>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image
+            src={`https://moshnogorskaia-next-js-user-images.s3.eu-north-1.amazonaws.com/${meal.image}`}
+            alt={meal.title}
+            fill
+          />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 }
--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -8,7 +8,7 @@ export default function MealItem({ title, slug, image, summary, creator }) {
     <article className={classes.meal}>
       <header>
         <div className={classes.image}>
-          <Image src={image} alt={title} fill />
+          <Image src={`https://moshnogorskaia-next-js-user-images.s3.eu-north-1.amazonaws.com/${image}`} alt={title} fill />
         </div>
         <div className={classes.headerText}>
           <h2>{title}</h2>
